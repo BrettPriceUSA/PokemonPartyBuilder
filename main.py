16a88e1fe4ffc9d58ab13e0d64f9caae56b3ce01
Brett Price <172153717+BrettPriceUSA@users.noreply.github.com>
2026-07-06 14:06:55 -0400

    added a starter list in PokeDex Class

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 class PokeDex:
+
     def __init__(self, name: str, type : str , id : int):
         self.name = name
         self.type = type
         self.id = id
+        
+        DexList = []
 
 
 class Pokemon(PokeDex):
 
 
-    def __init__(self, name: str , type: str , id : int, level: int, hp: int):
-        super().__init__(name, type, id)
+    def __init__(self, name: str , type: str , id : int, level: int):
 
+        self.name = name
+        self.type = type
+        self.id = id
         self.level = level
-        self.hp = hp
 
     def pokeStats(self):
         print(self.name, self.type, self.level, self.hp, self.id)
 
 
 
-pikachu = Pokemon('Pikachu', 'Electric', 5, 15, 25)
+DexList.append(Pokemon('Pikachu', 'Electric', 5, 15, 25))
 bulbasaur = Pokemon("Bulbasaur", "Grass", 5, 20, 1)
 charmander = Pokemon("Charmander", "Fire", 5, 15, 4)
 squirtle = Pokemon("Squirtle", "Water", 5, 20, 7)

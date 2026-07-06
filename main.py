7dd517480ab00500feaf0bedcd5da0a9dbf58774
Brett Price <172153717+BrettPriceUSA@users.noreply.github.com>
2026-07-06 13:48:49 -0400

    Added Pokedex class and additional pokemon

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-class Pokemon:
-    def __init__(self, name, type, level, hp):
+class PokeDex:
+    def __init__(self, name: str, type : str , id : int):
         self.name = name
         self.type = type
+        self.id = id
+
+
+class Pokemon(PokeDex):
+
+
+    def __init__(self, name: str , type: str , id : int, level: int, hp: int):
+        super().__init__(name, type, id)
+
         self.level = level
         self.hp = hp
 
-    def DexRead(self):
-        pass
+    def pokeStats(self):
+        print(self.name, self.type, self.level, self.hp, self.id)
 
 
 
-pikachu = Pokemon('Pikachu', 'Electric', 5, 15)
-print (pikachu.level)
+pikachu = Pokemon('Pikachu', 'Electric', 5, 15, 25)
+bulbasaur = Pokemon("Bulbasaur", "Grass", 5, 20, 1)
+charmander = Pokemon("Charmander", "Fire", 5, 15, 4)
+squirtle = Pokemon("Squirtle", "Water", 5, 20, 7)
+
+pikachu.pokeStats()
+bulbasaur.pokeStats()
+charmander.pokeStats()
+squirtle.pokeStats
